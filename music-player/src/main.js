bab95e22f3037513ef79150e73203c42c37123d9
AcePeQ <maciej.nojszewski22@gmail.com>
2025-07-05 17:26:15 +0200

    feat: add feature of playing / pausing the song and updating progress bar

--- music-player/src/main.js
+++ music-player/src/main.js
@@ -1,4 +1,4 @@
-"use sctrict";
+"use strict";
 
 const songs = [
   {
@@ -10,7 +10,7 @@ const songs = [
   {
     title: "Forest Lullaby",
     author: "Lesfm",
-    src: "forest-lullaby-110624",
+    src: "forest-lullaby-110624.mp3",
     img: "/cover-2.jpg",
   },
 ];
@@ -18,41 +18,78 @@ const songs = [
 let currentSongIndex = 0;
 let audio = new Audio(songs[0].src);
 
+const buttonPlayPause = document.getElementById("button_play");
+const buttonPlayPauseImage = document.getElementById("playPauseImage");
+const songCurrentTimeText = document.querySelector("#song_time_current");
+
+const progressBar = document.getElementById("song_progress_bar");
+
+buttonPlayPause.addEventListener("click", playPause);
+audio.addEventListener("timeupdate", updateProgressBar);
+
 function secondsToMs(duration) {
   duration = Number(duration);
+
+  if (duration === 0) return "00:00";
 
   const minutes = Math.floor((duration % 3600) / 60);
   const seconds = Math.floor((duration % 3600) % 60);
 
   const songTime =
-    minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart();
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
   return songTime;
 }
 
 function updateUI() {
   const currentSong = songs[currentSongIndex];
 
+  document.querySelector("#song_author").textContent = currentSong.author;
+  document.querySelector("#song_name").textContent = currentSong.title;
+
   const songImage = document.querySelector("#song_image");
-  const authorCaption = document.querySelector("#song_author");
-  const songNameCaption = document.querySelector("#song_name");
-  const songTimeText = document.querySelector("#song_time");
-
-  authorCaption.textContent = currentSong.author;
-  songNameCaption.textContent = currentSong.title;
-
   songImage.setAttribute("src", currentSong.img);
   songImage.setAttribute(
     "alt",
     `poster of ${currentSong.author} song: ${currentSong.title}`
   );
 
-  const songDuration = secondsToMs(audio.duration);
-  songTimeText.textContent = songDuration;
+  document.querySelector("#song_time").textContent = secondsToMs(
+    audio.duration
+  );
+  songCurrentTimeText.textContent = secondsToMs(audio.currentTime);
+
+  progressBar.setAttribute("max", audio.duration);
+  progressBar.setAttribute("value", 0);
 }
 
-function updateProgressBar() {}
+function updateProgressBar() {
+  if (audio.ended) {
+    progressBar.setAttribute("value", 0);
+    songCurrentTimeText.textContent = secondsToMs(0);
+    buttonPlayPauseImage.setAttribute("src", "/Play_fill.svg");
+    return;
+  }
+
+  progressBar.setAttribute("value", audio.currentTime);
+  songCurrentTimeText.textContent = secondsToMs(audio.currentTime);
+}
+
+async function playPause() {
+  const isAudioPaused = audio.paused;
+
+  if (!isAudioPaused) {
+    audio.pause();
+    buttonPlayPauseImage.setAttribute("src", "/Play_fill.svg");
+
+    return;
+  }
+
+  await audio.play();
+  buttonPlayPauseImage.setAttribute("src", "/Pause_fill.svg");
+}
 
 audio.addEventListener("loadedmetadata", () => {
-  console.log(audio);
   updateUI();
 });
